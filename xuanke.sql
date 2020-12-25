18ee98eaac1564ad6e55cadd9016b9e219b25fb4
CKJay123 <63487037+CKJay123@users.noreply.github.com>
2020-12-25 21:54:33 +0800

    Add files via upload

---
+++ xuanke.sql
@@ -0,0 +1,73 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.40 : Database - xuanke
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`xuanke` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci */;
+
+USE `xuanke`;
+
+/*Table structure for table `course` */
+
+DROP TABLE IF EXISTS `course`;
+
+CREATE TABLE `course` (
+  `course_id` int(11) NOT NULL AUTO_INCREMENT,
+  `coursename` varchar(20) COLLATE utf8_general_mysql500_ci DEFAULT NULL,
+  `teachar` varchar(20) COLLATE utf8_general_mysql500_ci DEFAULT NULL,
+  `number` int(60) DEFAULT NULL,
+  PRIMARY KEY (`course_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
+
+/*Data for the table `course` */
+
+insert  into `course`(`course_id`,`coursename`,`teachar`,`number`) values (1,'JavaEE','罗静',1);
+
+/*Table structure for table `user_course` */
+
+DROP TABLE IF EXISTS `user_course`;
+
+CREATE TABLE `user_course` (
+  `user_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`course_id`),
+  KEY `FK_Reference_2` (`course_id`),
+  CONSTRAINT `FK_Reference_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `FK_Reference_2` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
+
+/*Data for the table `user_course` */
+
+insert  into `user_course`(`user_id`,`course_id`) values (1,1);
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) COLLATE utf8_general_mysql500_ci DEFAULT NULL,
+  `password` varchar(100) COLLATE utf8_general_mysql500_ci DEFAULT NULL,
+  `realname` varchar(100) COLLATE utf8_general_mysql500_ci DEFAULT NULL,
+  `phone` varchar(11) COLLATE utf8_general_mysql500_ci DEFAULT NULL,
+  `role` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
+
+/*Data for the table `users` */
+
+insert  into `users`(`user_id`,`username`,`password`,`realname`,`phone`,`role`) values (1,'admin','123456','程康泽','16607245320',0);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
